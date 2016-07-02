d32fbfc401abc15d28febc5055b8f56703f3d8dc
Ab Cd <abcd@gmail.com>
2016-07-02 17:56:56 +0800

    add servo control

--- projects/rotary_encoder/rotary_encoder.ino
+++ projects/rotary_encoder/rotary_encoder.ino
@@ -5,7 +5,11 @@
   * v. 0.1 - very basic functions - mw 20061220
   *
   */  
+/* add servo motion now */
 
+#include <Servo.h> 
+
+Servo myservo;
 
  int val; 
  int encoder0PinA = 3;
@@ -14,24 +18,33 @@
  int encoder0PinALast = LOW;
  int n = LOW;
  int count = 0;
+ int servoPos = 0;
  
  void setup() { 
    pinMode (encoder0PinA,INPUT);
    pinMode (encoder0PinB,INPUT);
+   myservo.attach(9);  // attaches the servo on pin 9 to the servo object 
    Serial.begin (9600);
  } 
 
  void loop() { 
-   char stringBuffer[100];
    n = digitalRead(encoder0PinA);
    if ((encoder0PinALast == LOW) && (n == HIGH)) {
      count++;
      if (digitalRead(encoder0PinB) == LOW) {
        encoder0Pos--;
+       servoPos += 5;
+       if (servoPos > 179)
+         servoPos = 0;
      } else {
        encoder0Pos++;
+       servoPos -= 5;
+       if (servoPos < 0)
+         servoPos = 179;
      }
      Serial.println (encoder0Pos);
+     myservo.write(servoPos); 
+     delay(15);
    } 
    encoder0PinALast = n;
  } 
